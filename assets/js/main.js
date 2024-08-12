299f17a7d9f6eb62fd9fc2dab5e8eac37247b34e
immark007 <immark5507@gmail.com>
2024-08-11 22:02:40 -0300

    Atualizando mais uma vez, novamente, eternamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('#mobile_btn').on('click', function(){
+$(document).ready(function() {
+    $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find('i').toggleClass('fa-x');
     });
@@ -7,7 +7,7 @@ $(document).ready(function(){
     const sections = $('section');
     const navItems = $('.nav-item');
 
-    $(window).on('scroll', function(){
+    $(window).on('scroll', function() {
         const header = $('header');
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
         let activeSectionIndex = 0;
@@ -21,7 +21,7 @@ $(document).ready(function(){
         sections.each(function(i) {
             const section = $(this);
             const sectionTop = section.offset().top - 96;
-            const sectionBottom = sectionTop+ section.outerHeight();
+            const sectionBottom = sectionTop + section.outerHeight();
 
             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
@@ -33,45 +33,59 @@ $(document).ready(function(){
         $(navItems[activeSectionIndex]).addClass('active');
     });
 
-    ScrollReveal().reveal('.home_title',{
-        origin: 'left',
-        duration: 1000,
-        distance: '10%'
-    });
+    function initializeScrollReveal() {
+        ScrollReveal().reveal('.home_title', {
+            origin: 'left',
+            duration: 2000,
+            distance: '10%'
+        });
 
-    ScrollReveal().reveal('.conteudo h2', {
-        origin: 'left',
-        duration: 1000,
-        distance: '10%'
-    });
+        ScrollReveal().reveal('.conteudo h2', {
+            origin: 'left',
+            duration: 2000,
+            distance: '10%'
+        });
 
-    ScrollReveal().reveal('.conteudo span', {
-        origin: 'right',
-        duration: 1000,
-        distance: '10%'
-    });
+        ScrollReveal().reveal('.conteudo span', {
+            origin: 'right',
+            duration: 2000,
+            distance: '10%'
+        });
 
-    ScrollReveal().reveal('.construct_title',{
-        origin: 'left',
-        duration: 1000,
-        distance: '10%'
-    });
+        ScrollReveal().reveal('.construct_title', {
+            origin: 'left',
+            duration: 2000,
+            distance: '10%'
+        });
 
-    ScrollReveal().reveal('.keep-title',{
-        origin: 'right',
-        duration: 1000,
-        distance: '10%'
-    });
+        ScrollReveal().reveal('.keep-title', {
+            origin: 'right',
+            duration: 2000,
+            distance: '10%'
+        });
 
-    ScrollReveal().reveal('.construct_image',{
-        origin: 'right',
-        duration: 1000,
-        distance: '10%'
-    });
+        ScrollReveal().reveal('.construct_image', {
+            origin: 'right',
+            duration: 2000,
+            distance: '10%'
+        });
 
-    ScrollReveal().reveal('.keep-image',{
-        origin: 'left',
-        duration: 1000,
-        distance: '10%'
+        ScrollReveal().reveal('.keep-image', {
+            origin: 'left',
+            duration: 2000,
+            distance: '10%'
+        });
+    }
+
+    function checkScreenSizeAndInitialize() {
+        if (window.innerWidth > 768) { 
+            initializeScrollReveal();
+        }
+    }
+
+    checkScreenSizeAndInitialize();
+
+    $(window).on('resize', function() {
+        checkScreenSizeAndInitialize();
     });
 });
